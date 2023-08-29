9442d29527f5738db2fa98383ba14dee593005a6
Todd Griffin <todd.griffin61@outlook.com>
2023-08-29 14:19:29 +0100

    added line graph for steps

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -2,9 +2,48 @@ import { useState, useEffect } from'react'
 import '../../App.css'
 import './style.css'
 
-export default function Dashboard() {
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+import { Line } from 'react-chartjs-2';
 
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+const options = {
+  scales: {
+    y: {
+      beginAtZero: true
+    }
+  },
+  plugins: {
+    legend: {
+      position: 'top'
+    },
+    title: {
+      display: true,
+      text: 'Chart.js Line Chart',
+    },
+  },
+};
+
+export default function Dashboard() {
   const [ habits, setHabits ] = useState([])
+  const [ chart1Labels, setChart1Labels ] = useState([])
 
   async function getHabits() {
     const domain = process.env.REACT_APP_DB_DOMAIN
@@ -22,6 +61,31 @@ export default function Dashboard() {
     getHabits()
   }, [])
 
+  // This use effect will generate chart labels from the habits variable
+  useEffect(() => {
+    const labels = []
+    for (let i=0; i<habits.length; i++) {
+      labels.unshift(habits[i].date)
+    }
+    console.log(labels.slice(-10))
+    setChart1Labels(labels.slice(-10))
+  }, [habits])
+
+  const chart1Data = {
+    labels: chart1Labels,
+    datasets: [
+      {
+        label: 'Steps',
+        data: habits.map((entry) => { 
+          return entry.steps 
+        }).reverse().slice(-10),
+        borderColor: 'rgb(255, 99, 132)',
+        backgroundColor: 'rgba(255, 99, 132, 0.5)'
+      }
+    ]
+  }
+
+  console.log(chart1Data.datasets[0].data)
 
   const getSleepAverage = () => {
     let sum = 0
@@ -71,6 +135,8 @@ export default function Dashboard() {
         <h3>Average sleep this week</h3>
         <p><span className="entry-value">{getSleepAverage()}</span> hours</p>
       </div>
+      <Line options={options} data={chart1Data} />
+      {console.log( <Line options={options} data={chart1Data} />)}
     </>
   )
 }
